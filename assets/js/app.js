a9e02a09dcb5d4472de9042c30363a067e4fcc8d
luismart <luismart@live.cl>
2024-02-14 15:45:02 -0400

    ricomida final

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,41 @@
-$(function () {
+const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
+const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
 
-    $("button").click(function () {
-        alert("hola");
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+$(document).ready(function () {
+
+    $("h2").dblclick(function () {
+        $(this).css({
+            "color": "red",
+
+        });
+    });
+});
+
+document.getElementById("boton1").onclick = function () {
+    alert("El correo fue enviado correctamente...");
+}
+
+document.getElementById("boton2").onclick = function () {
+    alert("Añadido a tus favoritos...");
+}
+
+$(document).ready(function () {
+    $("#recipe1").click(function () {
+        $(this).toggle("slow");
+    });
+
+    $("#recipe2").click(function () {
+        $(this).toggle("slow");
+    });
+
+    $("#recipe3").click(function () {
+        $(this).toggle("slow");
     });
 
 });
+
+
+
